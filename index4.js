f9cba9b3c8924b0ceceae52d874c0cd5e84731c0
guest81 <italyacc86@gmail.com>
2026-02-05 00:43:53 +0200

    Update index4.js

--- index4.js
+++ index4.js
@@ -1,33 +1,41 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 const axios = require("axios");
+const express = require("express");
 
 const TOKEN = process.env.TOKEN;
 
 const API = "https://roblox-api-production-08e4.up.railway.app/count";
 const CHANNEL_ID = "1468033384176423064";
 
-const INTERVAL = 3000; // كل 3 ثواني
+const INTERVAL = 3000;
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
+// ✅ سيرفر صغير للـ ping
+const app = express();
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Ping server running"));
+
 let messageNow = null;
 let messageBest = null;
-
 let bestCount = 0;
 
-client.once("ready", async () => {
+client.once("clientReady", async () => {
   console.log("Bot ready!");
 
   const channel = await client.channels.fetch(CHANNEL_ID);
 
-  // رسالة العدد الحالي
   messageNow = await channel.send(
     "PhantomX Online Users Now: **None**"
   );
 
-  // رسالة أعلى عدد
   messageBest = await channel.send(
     "Best Number Of PhantomX Users: **0**"
   );
@@ -37,14 +45,12 @@ client.once("ready", async () => {
       const res = await axios.get(API);
       const count = Number(res.data.online) || 0;
 
-      // تحديث رسالة العدد الحالي دائمًا
       await messageNow.edit(
         `PhantomX Online Users Now: **${count}**`
       );
 
       client.user.setActivity(`Online: ${count}`);
 
-      // تحديث أفضل رقم
       if (count > bestCount) {
         bestCount = count;
 
